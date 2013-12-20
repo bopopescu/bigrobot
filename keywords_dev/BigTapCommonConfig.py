03fa4aaffe429fb29f64261b5e8ce5fc782eaf1f
Animesh Patcha <animesh.patcha@bigswitch.com>
2013-12-20 14:33:05 -0800

    In Flight

---
+++ keywords_dev/BigTapCommonConfig.py
@@ -0,0 +1,306 @@
+import autobot.helpers as helpers
+import autobot.test as test
+
+
+class BigTapCommonConfig(object):
+
+    def __init__(self):
+        t = test.Test()
+        c = t.controller()
+
+        url = '%s/auth/login' % c.base_url
+        
+        helpers.log("url: %s" % url)
+        result = c.rest.post(url, {"user":"admin", "password":"adminadmin"})
+
+        helpers.log("result: %s" % helpers.to_json(result))
+
+        session_cookie = result['content']['session_cookie']
+        c.rest.set_session_cookie(session_cookie)
+        
+#Assign filter/delivery/service node characteristics to particular switch interfaces.
+#Same as bigtap role filter interface-name F1    
+    def rest_setup_bigtap_interfaces(self,swName,intfName,intfType,intfNick):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/interface-config[interface="%s"][switch="%s"]' % (c.base_url, str(intfName), str(swName))
+        c.rest.put(url, {"interface": str(intfName), "switch": str(swName), 'role':str(intfType),'name':str(intfNick)})
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+ 
+# Delete filter/service/delivery interface from switch configuration   
+    def rest_delete_interface_role(self,swName,intfName,intfType,intfNick):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/interface-config[interface="%s"][switch="%s"]' % (c.base_url, str(intfName), str(swName)) 
+        c.rest.delete(url, {'role':str(intfType), "name": str(intfNick)})
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+#            
+    def rest_delete_interface(self,swName,intfName,intfType):
+        t = test.Test()
+        c = t.controller()
+        url1 = '%s/api/v1/data/controller/core/switch[dpid="%s"]/interface[name=""]'  % (c.base_url, str(swName), str(intfName))
+        c.rest.delete(url1, {})
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+    def rest_add_policy(self,viewName,policyName,policyAction):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]' % (c.base_url, str(viewName), str(policyName))
+        c.rest.put(url,{'name':str(policyName)})
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+        c.rest.patch(url,{"action": str(policyAction) })
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+    def rest_delete_policy(self,viewName,policyName):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]' % (c.base_url, str(viewName), str(policyName))
+        c.rest.delete(url,{})
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+    def rest_add_policy_interface(self,viewName,policyName,intfNick,intfType):
+        t = test.Test()
+        c = t.controller()
+        if "filter" in str(intfType) :
+            intf_type = "filter-group"
+        else :
+            intf_type = "delivery-group"
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/%s[name="%s"]' % (c.base_url, str(viewName), str(policyName),str(intf_type),str(intfNick))
+        c.rest.put(url,{"name": str(intfNick)})
+        helpers.sleep(1)
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+    def rest_delete_policy_interface(self,viewName,policyName,intfNick,intfType):
+        t = test.Test()
+        c = t.controller()
+        if "filter" in str(intfType) :
+            intf_type = "filter-group"
+        else :
+            intf_type = "delivery-group"
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/%s[name="%s"]' % (c.base_url, str(viewName), str(policyName),str(intf_type),str(intfNick))
+        c.rest.delete(url,{})
+        helpers.sleep(1)
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+# Add a match condition to a policy    
+    def rest_add_policy_match(self,viewName,policyName,match_number,data):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/rule[sequence=%s]'  % (c.base_url,str(viewName),str(policyName),str(match_number))
+        data_dict = helpers.from_json(data)
+        c.rest.put(url,data_dict)
+        helpers.test_log(c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_log(c.rest.content_json())
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+# delete a match condition from a policy
+    def rest_delete_policy_match(self,viewName,policyName,match_number):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/rule[sequence="%s"]'  % (c.base_url,str(viewName),str(policyName),str(match_number))
+        c.rest.delete(url,{})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+
+
+# Add a service with Pre-Service and Post Service interface.
+    def rest_add_bigtap_service(self,serviceName,intfPreNick,intfPostNick):
+        t = test.Test()
+        c = t.controller()
+        #Add Service
+        url = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]' % (c.base_url,str(serviceName))
+        c.rest.put(url,{"name":str(serviceName)})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else :
+            helpers.test_log(c.rest.content_json())
+        #Add Pre-Service Interface
+        url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/pre-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfPreNick))
+        c.rest.put(url_add_intf, {"name":str(intfPreNick)})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else :
+            helpers.test_log(c.rest.content_json())
+        #Add Post-Service Interface
+        url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/post-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfPostNick))
+        c.rest.put(url_add_intf, {"name":str(intfPostNick)})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+ 
+# Delete a service
+    def rest_delete_bigtap_service(self,serviceName) :
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]'  % (c.base_url,str(serviceName))
+        c.rest.delete(url,{})   
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+        return True
+ 
+#Add interface to service
+    def rest_add_interface_service(self,serviceName,intfType,intfNick):
+        t = test.Test()
+        c = t.controller()
+        if "pre" in str(intfType) :
+            url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/pre-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfNick))
+        else :
+            url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/post-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfNick))
+        c.rest.post(url_add_intf, {"name":str(intfNick)})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+#Delete interface from service
+    def rest_delete_bigtap_service_intf(self,serviceName,intfNick,intfType) :
+        t = test.Test()
+        c = t.controller()
+        if "pre" in str(intfType) :
+            url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/pre-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfNick))
+        else :
+            url_add_intf = '%s/api/v1/data/controller/applications/bigtap/service[name="%s"]/post-group[name="%s"]'  % (c.base_url,str(serviceName),str(intfNick))
+        c.rest.delete(url_add_intf, {})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+#Add service to policy
+    def rest_add_service_to_policy(self,viewName,policyName,serviceName,sequenceNumber) :
+        t = test.Test()
+        c = t.controller()
+        url_to_add = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/service[sequence="%s"]' % (c.base_url,str(viewName),str(policyName),str(sequenceNumber))
+        c.rest.put(url_to_add, {"name":str(serviceName), "sequence" : sequenceNumber})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+#Delete service from policy
+    def rest_delete_service_from_policy(self,viewName,policyName,serviceName) :
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]/service[name="%s"]' % (c.base_url,str(viewName),str(policyName),str(serviceName))
+        c.rest.delete(url, {})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+#Change policy action
+    def rest_change_policy_action(self,viewName,policyName,policyAction):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view[name="%s"]/policy[name="%s"]' % (c.base_url,str(viewName),str(policyName))
+        c.rest.patch(url,{"action":str(policyAction)})
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+
+#Disable bigtap feature overlap/inport-mask/tracked-host/l3-l4-mode
+    def rest_disable_feature(self,featureName):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/feature'  % (c.base_url)
+        c.rest.patch(url,{str(featureName): False})
+        helpers.sleep(1)
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+    
+#Enable bigtap feature overlap/inport-mask/tracked-host/l3-l4-mode
+    def rest_enable_feature(self,featureName):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/feature'  % (c.base_url)
+        c.rest.patch(url,{str(featureName): True})
+        helpers.sleep(1)
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+            return False
+        else:
+            helpers.test_log(c.rest.content_json())
+            return True
+        
+#Compare coreswitch flows
+    def rest_compare_coreswitch_flows(self,flow1,flow2,flowVal1,flowVal2):
+        if ((flow1 == flowVal1) and (flow2 == flowVal2 )) or ((flow2 == flowVal1) and (flow1 == flowVal2 )) :
+            return True
+        else :
+            return False
+
---
+++ keywords_dev/BigTapCommonShow.py
@@ -0,0 +1,117 @@
+import autobot.helpers as helpers
+import autobot.test as test
+
+
+class BigTapCommonShow(object):
+
+    def __init__(self):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/auth/login' % c.base_url
+        helpers.log("url: %s" % url)
+        result = c.rest.post(url, {"user":"admin", "password":"adminadmin"})
+        helpers.log("result: %s" % helpers.to_json(result))
+        session_cookie = result['content']['session_cookie']
+        c.rest.set_session_cookie(session_cookie)
+        
+#Return Controller Role
+    def rest_ha_role(self):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/system/ha/role'  % (c.base_url)
+        c.rest.get(url)
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+        content = c.rest.content()
+        return content['role']
+        
+    def rest_show_bigtap_policy(self, policyName,numFIntf,numDIntf):
+        t = test.Test()
+        c = t.controller()
+        helpers.sleep(1)
+        helpers.test_log("Input arguments: policy = %s" % policyName )
+        url = '%s/api/v1/data/controller/applications/bigtap/view/policy[name="%s"]/info' % (c.base_url, policyName)
+        c.rest.get(url)
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+        content = c.rest.content()
+                
+        if content[0]['name'] == str(policyName):
+                helpers.test_log("Policy correctly reports policy name as : %s" % content[0]['name'])
+        else:
+                helpers.test_failure("Policy does not correctly report policy name  : %s" % content[0]['name'])                
+                return False        
+        if content[0]['config-status'] == "active and forwarding":
+                helpers.test_log("Policy correctly reports config status as : %s" % content[0]['config-status'])
+        else:
+                helpers.test_failure("Policy does not correctly report detailed status as : %s" % content[0]['delivery-interface-count'])
+                return False                
+        if content[0]['delivery-interface-count'] == int(numDIntf) :
+                helpers.test_log("Policy correctly reports number of delivery interfaces as : %s" % content[0]['delivery-interface-count'])
+        else:
+                helpers.test_failure("Policy does not correctly report number of delivery interfaces  : %s" % content[0]['delivery-interface-count'])                
+                return False                
+        if content[0]['filter-interface-count'] == int(numFIntf):
+                helpers.test_log("Policy correctly reports number of filter interfaces as : %s" % content[0]['filter-interface-count'])
+        else:
+                helpers.test_failure("Policy does not correctly report number of filter interfaces  : %s" % content[0]['filter-interface-count'])                
+                return False
+            
+        if content[0]['detailed-status'] == "installed to forward":
+                helpers.test_log("Policy correctly reports detailed status as : %s" % content[0]['detailed-status'])
+        else:
+                helpers.test_failure("Policy does not correctly report detailed status as : %s" % content[0]['detailed-status'])
+                return False
+        return True
+
+# Do a rest query and check if a particular key exists in the policy.
+# index is the index of the dictionary.
+# Values for method are info/rule/filter-interface/delivery-interface/service-interface/core-interface/failed-paths    
+    def rest_check_policy_key(self,policyName,method,index,key):
+        t = test.Test()
+        c = t.controller()
+        url = '%s/api/v1/data/controller/applications/bigtap/view/policy[name="%s"]/%s' % (c.base_url,str(policyName),str(method))
+        c.rest.get(url)
+        helpers.test_log("Ouput: %s" % c.rest.result_json())
+        if not c.rest.status_code_ok():
+            helpers.test_failure(c.rest.error())
+        if(c.rest.content()):
+            content = c.rest.content()
+            return content[index][key]
+        else :
+            helpers.test_log("ERROR Policy %s does not exist. Error seen: %s" % (str(policyName),c.rest.result_json()))
+            return False
+        
+# Return switch dpid given a switch alias
+# Input: switch alias
+# Output: switch dpid 
+    def rest_show_switch_dpid(self,switchAlias):
+        t = test.Test()
+        c = t.controller()
+        aliasExists=0
+        url='%s/api/v1/data/controller/core/switch?select=alias'   % (c.base_url)
+        c.rest.get(url)
+        content = c.rest.content()
+        for i in range(0,len(content)) :
+                if content[i]['alias'] == str(switchAlias) :
+                        switchDpid = content[i]['dpid']
+                        aliasExists=1
+        if(aliasExists):
+            return switchDpid
+        else:
+            return False
+        
+# Return number of flows installed in full match table
+# Input: switch dpid
+# Output: Number of flows      
+    def rest_show_switch_flow(self,switchDpid):
+        t = test.Test()
+        c = t.controller()
+        url='%s/api/v1/data/controller/core/switch[dpid="%s"]?select=stats/table' % (c.base_url,str(switchDpid))
+        c.rest.get(url)
+        content = c.rest.content()
+        helpers.log("Return value for number of flows is %s" % content[0]['stats']['table'][1]['active-count'])
+        return content[0]['stats']['table'][1]['active-count']      
+      
