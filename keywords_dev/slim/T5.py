1c900574e47ed5e37423149ee695b1539c86c647
SongBeng Lim <songbeng.lim@bigswitch.com>
2014-01-16 13:19:36 -0800

    update

--- keywords_dev/slim/T5.py
+++ keywords_dev/slim/T5.py
@@ -433,7 +433,7 @@ class T5(object):
         else: 
             helpers.test_log("Output: %s" % c.rest.result_json())
             return c.rest.content()        
-        
+    
     def rest_add_static_routes(self, tenant, dstroute, nexthop):
         '''Add static routes to tenant router"
         
@@ -448,11 +448,13 @@ class T5(object):
         t = test.Test()
         c = t.controller()
         
+        nexthop_dict = helpers.from_json(nexthop)
+        
         helpers.test_log("Input arguments: tenant = %s dstroute = %s nexthop = %s " % (tenant, dstroute, nexthop))
         
         url = '%s/api/v1/data/controller/applications/bvs/tenant[name="%s"]/virtual-router/routes' % (c.base_url, tenant)
         try:
-            c.rest.post(url, {"dest-ip-subnet": dstroute, "next-hop": nexthop})
+            c.rest.post(url, {"dest-ip-subnet": dstroute, "next-hop": nexthop_dict})
         except:
             helpers.test_failure(c.rest.error())
         else: 
