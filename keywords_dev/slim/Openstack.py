a9eb51b89144832fddcf95c87ee92a521ab25301
SongBeng Lim <songbeng.lim@bigswitch.com>
2014-01-16 07:39:22 -0800

    update openstack.py

--- keywords_dev/slim/Openstack.py
+++ keywords_dev/slim/Openstack.py
@@ -7,9 +7,19 @@ class Openstack(object):
     def __init__(self):
         pass
     
-    
-    def create_tenant(self, osUserName, osTenantName, osAuthUrl, osPassWord, tenantName):        
+    def openstack_get_image_id(self, osUserName, osTenantName, osAuthUrl, osPassWord, imageName):
         t = test.Test()
-        nova = t.nova()
-        mn.cli('%s ping %s -c %s' % (src, dst, count))
+        h1 = t.host('h1')
+    
+        result = h1.bash("nova --os-username %s --os-tenant-name %s  --os-auth-url %s --os-password %s image-show %s | grep -i id" % (osUserName, osTenantName, osAuthUrl, osPassWord, imageName))
+        a = result.split("|")
+        return a[1]
+    
+#    def openstack_create_tenant(self, osUserName, osTenantName, osAuthUrl, osPassWord, tenantName):        
+#        t = test.Test()
+#        h1 = t.host('h1')
+#        h2 = t.host('h2')
+        
+        
+        
       
