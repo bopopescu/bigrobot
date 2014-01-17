0e1dab99528867589bc8d21292645905b63f29ed
Don Jayakody <donj@Dons-MacBook-Pro-3.local>
2014-01-16 18:39:51 -0800

    Changing T5Platform.py to correct tabbing

--- keywords_dev/T5Platform.py
+++ keywords_dev/T5Platform.py
@@ -39,36 +39,39 @@ class T5Platform(object):
         
         return True
     
+    def rest_verify_ha_cluster(self):
+        try:
+
     
     def rest_verify_ha_cluster(self):
         '''Using the 'show cluster' command verify the cluster formation across both nodes
 	   Also check for the formation integrity
-	'''	
+	'''
 	try:
-		t = test.Test()
-		master = t.controller("master")
-		slave = t.controller("slave")
-		url = '/api/v1/data/controller/cluster'
-		
-		result = master.rest.get(url)['content']
-		reported_active_byMaster = result[0]['status']['domain-leader']['leader-id']
-
-		result = slave.rest.get(url)['content']
-		reported_active_bySlave = result[0]['status']['domain-leader']['leader-id']
-		
-		if(reported_active_byMaster != reported_active_bySlave):
-			helpers.log("Both controllers %s & %s are declaring themselves as active" \
-					% (reported_active_byMaster, reported_active_bySlave))
-			helpers.test_failure("Error: Inconsistent active/stand-By cluster formation detected")
-			return False
-		else:
-			helpers.log("Active controller id is: %s " % reported_active_byMaster)
-			helpers.log("Pass: Consistent active/stand-By cluster formation verified")
-			return True
+            t = test.Test()
+            master = t.controller("master")
+            slave = t.controller("slave")
+            url = '/api/v1/data/controller/cluster'
+            
+            result = master.rest.get(url)['content']
+            reported_active_byMaster = result[0]['status']['domain-leader']['leader-id']
+            
+            result = slave.rest.get(url)['content']
+            reported_active_bySlave = result[0]['status']['domain-leader']['leader-id']
+            
+            if(reported_active_byMaster != reported_active_bySlave):
+                helpers.log("Both controllers %s & %s are declaring themselves as active" \
+                        % (reported_active_byMaster, reported_active_bySlave))
+		helpers.test_failure("Error: Inconsistent active/stand-By cluster formation detected")
+                return False
+            else:
+                helpers.log("Active controller id is: %s " % reported_active_byMaster)
+                helpers.log("Pass: Consistent active/stand-By cluster formation verified")
+                return True
 
 	except Exception, err:
-		helpers.test_failure("Exception in: rest_verify_ha_cluster %s : %s " % (Exception, err))
-		return False
+            helpers.test_failure("Exception in: rest_verify_ha_cluster %s : %s " % (Exception, err))
+            return False
 
 
 	        
