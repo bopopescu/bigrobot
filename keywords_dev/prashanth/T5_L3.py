69dd674d08ceb46d5e367ebfcd6ab3ff96fc6e89
SongBeng Lim <songbeng.lim@bigswitch.com>
2014-02-04 08:45:12 -0800

    update

--- keywords_dev/prashanth/T5_L3.py
+++ keywords_dev/prashanth/T5_L3.py
@@ -239,6 +239,22 @@ REST-POST: DELETE http://127.0.0.1:8080/api/v1/data/controller/applications/bvs/
         data = c.rest.content()
         return data
     
+    def rest_show_endpoints_mac(self, mac):
+        '''
+        %5Bmac%3D%2200%3A00%3A00%3A00%3A00%3A01%22%5D
+        '''
+        t = test.Test()
+        c = t.controller()
+        
+        str1 = mac.replace(":", "%3A")
+        mac_addr = "%5Bmac%3D%22" + str1 + "%22%5D" 
+        url = '%s/api/v1/data/controller/applications/bvs/info/endpoint-manager/endpoints%s' % (c.base_url, mac_addr)
+        c.rest.get(url)
+        helpers.log("Output: %s" % c.rest.result_json())
+        data = c.rest.content()
+        return data
+    
+    
     def rest_count_endpoints_mac(self):
         data = self.rest_show_endpoints()
         return len(data)
