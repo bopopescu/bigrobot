fb9562c8c6108936454019627d89e2f708b0c76a
Vui Le <vui.le@bigswitch.com>
2014-02-25 10:03:08 -0800

    rev'ed up version

--- autobot/version.py
+++ autobot/version.py
@@ -2,7 +2,7 @@
 Follows the format defined by http://semver.org/
 """
 
-BIGROBOT_VERSION = "0.0.4"  # We're still in Beta
+BIGROBOT_VERSION = "0.0.5"  # We're still in Beta
 
 def get_version():
     return BIGROBOT_VERSION
