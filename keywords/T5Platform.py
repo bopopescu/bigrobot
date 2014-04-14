1b69dcb91cb43b8cdd7d1d74a407cf3fc6cc160c
Don Jayakody <donj@Dons-MacBook-Pro-8.local>
2014-04-14 14:35:05 -0700

    Platform related changes

--- keywords/T5Platform.py
+++ keywords/T5Platform.py
@@ -245,6 +245,7 @@ class T5Platform(object):
                 else:
                     helpers.log("Fail: Reboot Failed. Cluster is not stable. Before the master reboot Master is: %s / Slave is : %s \n \
                             After the reboot Master is: %s / Slave is : %s " %(masterID, slaveID, newMasterID, newSlaveID))
+                    obj.stop_floodlight_monitor()
                     return False
             else:
                 if(masterID == newMasterID and slaveID == newSlaveID):
@@ -253,6 +254,7 @@ class T5Platform(object):
                 else:
                     helpers.log("Fail: Reboot Failed. Cluster is not stable. Before the slave reboot Master is: %s / Slave is : %s \n \
                             After the reboot Master is: %s / Slave is : %s " %(masterID, slaveID, newMasterID, newSlaveID))
+                    obj.stop_floodlight_monitor()
                     return False
 
 
@@ -1827,34 +1829,6 @@ class T5Platform(object):
         else:
             helpers.test_failure(c.rest.error())      
  
-
-    def cli_get_num_nodes(self):
-        '''  
-          return the number of nodes in the system  
-          Author: Mingtao
-          input:                                        
-          usage:   
-          output:   1  or 2 
-        '''
-        t = test.Test()
-        c = t.controller('master')
-        helpers.log('INFO: Entering ==> rest_get_node_role ')
-        
-        c.cli('show cluster' )
-        content = c.cli_content()
-        temp = helpers.strip_cli_output(content)
-        temp = helpers.str_to_list(temp)
-        num = 0
-        for line in temp:          
-            helpers.log("INFO: line is - %s" % line)
-            match= re.match(r'.*(active|stand-by).*', line)
-            if match:
-                helpers.log("INFO: role is: %s" % match.group(1))  
-                num = num+1                                      
-            else:
-                helpers.log("INFO: not for controller  %s" % line)  
-        helpers.log("INFO: there are %d of controllers in the cluster" % num)   
-        return num    
 
     def cli_whoami(self):
         '''  
