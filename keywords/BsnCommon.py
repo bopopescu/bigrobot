9f0a855f409698931b543f6eeb3570e9149213ba
Vui Le <vui.le@bigswitch.com>
2014-05-15 10:27:50 -0700

    Cleaned up the postmortem code - trying to simplify controller_postmortem() as much as possible.

--- keywords/BsnCommon.py
+++ keywords/BsnCommon.py
@@ -109,12 +109,11 @@ class BsnCommon(object):
         Save the show commands and logs (e.g., /var/log/floodlight/*) to the
         archiver.
         """
-        dest_path += '/' + test_descr + '/' + node
-        server_devconf.sudo('mkdir -p %s' % dest_path)
-
         helpers.log("Collecting postmortem information for controller '%s'"
                     % node)
         output_dir = helpers.bigrobot_log_path_exec_instance()
+        # show_cmd_file is the log file on the BigRobot terminal (where
+        # test suite is running)
         show_cmd_file = (output_dir + '/' + test_descr + '_' + node +
                          '/show_cmd_out.txt')
         d = os.path.dirname(show_cmd_file)
@@ -197,13 +196,17 @@ class BsnCommon(object):
 
         h = t.node_spawn(ip=server, user=user, password=password,
                          device_type='host')
+
         for node in t.topology():
+            test_dest_path = dest_path + '/' + test_descr + '/' + node
+            h.sudo('mkdir -p %s' % test_dest_path)
+
             if helpers.is_controller(node):
                 self.controller_postmortem(node,
                                            server=server,
                                            server_devconf=h,
                                            user=user, password=password,
-                                           dest_path=dest_path,
+                                           dest_path=test_dest_path,
                                            test_descr=test_descr)
 
         # Only print the postmortem URL once.
