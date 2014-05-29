7fd151fd594b17a1cfc3164f231aeb24ecc61e93
Vui Le <vui.le@bigswitch.com>
2014-05-28 23:37:37 -0700

    Misc updates for Test Catalog

---
+++ catalog/mongoimport_suites_collection.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+database=test_catalog
+collection=test_suites
+mongo_server=qadashboard-mongo.bigswitch.com
+
+for json_file in `echo test_suites_*.json`; do
+    echo "*** Mongo importing ${json_file}..."
+    mongoimport \
+        --host $mongo_server \
+        --port 27017 \
+        --db $database \
+        --collection $collection \
+        --stopOnError \
+        --jsonArray \
+        --file $json_file
+done
+
+echo "Done."
