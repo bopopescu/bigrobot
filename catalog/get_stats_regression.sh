8f065c10c68397796a3b0f9ad0ef12f2560f5b50
Vui Le <vui.le@bigswitch.com>
2014-06-02 23:33:17 -0700

    Updated.

--- catalog/get_stats_regression.sh
+++ catalog/get_stats_regression.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
-# 'infile' contains a list of output.xml files (path included).
+# 'infile': a file which contains a list of output.xml files (path included).
 #
 
 ts=`date "+%Y-%m-%d_%H%M%S"`
-infile=regression_suites.out
-outfile=doit_regression.sh_output.$ts.log
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <infile>"
+fi
+
+infile=$1
+progname=`basename $0`
+outfile=${prog_name}_output.$ts.log
 
 rm -f test_suites_regression.json test_cases_regression.json
 
