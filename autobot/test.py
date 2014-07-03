25f8aed5d73ee63b3a2e1ded5ba4543d4e710a7f
amallina <aruna.mallina@bigswitch.com>
2014-07-03 14:40:05 -0700

    fix a logic for spine crash trace

--- autobot/test.py
+++ autobot/test.py
@@ -824,19 +824,15 @@ class Test(object):
             n_console.send(user)
             if helpers.bigrobot_test_ztn().lower() == 'true':
                 helpers.debug("Env BIGROBOT_TEST_ZTN is True. DO NOT EXPECT PASSWORD...")
-            match = n_console.expect(prompt=[prompt_password, prompt_device_cli, spine_stack_trace])
+            match = n_console.expect(prompt=[prompt_password, prompt_device_cli])
             if match[0] == 0:
                 helpers.log("Found the password prompt. Sending password.")
                 n_console.send(password)
                 match = n_console.expect(prompt=prompt_device_cli)
-            if match[2] == 0:
-                helpers.log("Found a switch Crash Needs a power cycle...")
-                helpers.log("Exiting the tests now ..Until Power cycle is added with new PDU's")
-                helpers.exit_robot_immediately("Needs power cycle of the switch that crashed..")
         n_console.send('')
 
         # Match login or CLI prompt.
-        match = n_console.expect(prompt=[prompt_login, prompt_device_cli])
+        match = n_console.expect(prompt=[prompt_login, prompt_device_cli, spine_stack_trace])
         if match[0] == 0:
             login()  # Found login prompt. Attempt to authenticate.
         elif match[0] == 1:
@@ -844,6 +840,10 @@ class Test(object):
             n_console.send('logout')
             match = n_console.expect(prompt=[prompt_login])
             login()
+        elif match[0] == 2:
+                helpers.log("Found a switch Crash Needs a power cycle...")
+                helpers.log("Exiting the tests now ..Until Power cycle is added with new PDU's")
+                helpers.exit_robot_immediately("Needs power cycle of the switch that crashed..")
 
         # Assume that the device mode is CLI by default.
         n_console.mode('cli')
