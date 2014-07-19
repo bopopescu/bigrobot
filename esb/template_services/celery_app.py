a2521f12e17a4c82438ce182bcec581fcfee1d29
Vui Le <vui.le@bigswitch.com>
2014-07-18 17:40:52 -0700

    - Refactored ESB code. - added esb/template_services/ (ESB template for sandbox development).

--- esb/template_services/celery_app.py
+++ esb/template_services/celery_app.py
@@ -2,15 +2,14 @@ from __future__ import absolute_import
 from celery import Celery
 
 # RabbitMQ Web GUI: http://qa-esb1.qa.bigswitch.com:15672/
-app = Celery('bsn_services',
+app = Celery('template_services',
              broker='amqp://guest@qa-esb1.qa.bigswitch.com//',
              backend='amqp://guest@qa-esb1.qa.bigswitch.com//',
-             include=['bsn_services.sample_method_tasks',
-                      'bsn_services.controller_upgrade'])
+             include=['template_services.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
-    CELERY_TASK_RESULT_EXPIRES=3600,
+    CELERY_TASK_RESULT_EXPIRES=3600,  # 1 hour
 )
 
 if __name__ == '__main__':
