056ea13feca877c8e08d8dfcfeb56072d729da76
Arun.Mallina <aruna.mallina@bigswitch.com>
2014-07-29 22:27:59 -0700

    debug cmds..

--- keywords/KVMOperations.py
+++ keywords/KVMOperations.py
@@ -216,7 +216,9 @@ class KVMOperations(object):
                 helpers.log("SCP should be done:\n%s" % scp_cmd_out)
             helpers.summary_log("Success SCP'ing latest Jenkins build !!")
         helpers.summary_log("Using Jenkins BCF Build #%s" % latest_build_number)
+        helpers.log("Setting BUILD_NUM Env...")
         helpers.set_env("BUILD_NUM", str(latest_build_number))
+        helpers.log("env BUILD_NUM: %s" % helpers.get_env("BUILD_NUM"))
         kvm_handle.bash('sudo cp %s ../images/%s.qcow2' % (file_name, vm_name))
 
         local_qcow_path = "/var/lib/libvirt/images/%s.qcow2" % vm_name
