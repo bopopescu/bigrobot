b41f63dff4565870d18b8b138791cb300897ec81
Vui Le <vui.le@bigswitch.com>
2014-09-10 11:54:36 -0700

    - test.py: avoid potential terminal garbage when changing to long   directory name (prompt gets corrupted). - tweaks to Test Catalog tools.

--- catalog/db_get_testcases_ironhorse.py
+++ catalog/db_get_testcases_ironhorse.py
@@ -5,6 +5,7 @@
 
 import os
 import sys
+import pymongo
 
 # Determine BigRobot path(s) based on this executable (which resides in
 # the bin/ directory.
@@ -20,10 +21,25 @@ from catalog_modules.test_catalog import TestCatalog
 if not 'BUILD_NAME' in os.environ:
     helpers.error_exit("Environment variable BUILD_NAME is not defined.", 1)
 
+release = 'ironhorse'
 build_name = os.environ['BUILD_NAME']
 cat = TestCatalog()
-testcases = cat.find_test_cases_matching_build(build_name)
+testsuites = cat.find_test_suites_matching_build(build_name)
+testcases = cat.find_test_cases_matching_build(build_name, release)
+
+product_suites = {}
+for testsuite in testsuites:
+    product_suites[testsuite["product_suite"]] = testsuite["author"]
+
 testcase_count = testcases.count()
 print("Total test cases: %d" % testcase_count)
+
+for testcase in testcases.sort([("product_suite", pymongo.ASCENDING),
+                                ("name", pymongo.ASCENDING)]):
+    product_suite = helpers.unicode_to_ascii(testcase["product_suite"])
+    testcase_name = helpers.unicode_to_ascii(testcase["name"])
+    author = product_suites[product_suite]
+    print("%-10s %-60s %s"
+          % (author, product_suite, testcase_name))
 
 sys.exit(0)
