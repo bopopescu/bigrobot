9de234d4d6bcc30b8d0609cdcad3778209cfb632
Arun.Mallina <aruna.mallina@bigswitch.com>
2014-09-11 19:18:02 -0700

    update typo

--- keywords/T5.py
+++ keywords/T5.py
@@ -2184,7 +2184,7 @@ class T5(object):
         result = c.cli_content()
         return result
 
-    def ssvecli_controller_reboot_switch(self, switch=None):
+    def cli_controller_reboot_switch(self, switch=None):
         '''Function to reboot switch from master controllers CLI
             if switch argument is not passed reboot all switchs
         '''
