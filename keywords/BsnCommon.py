668798217f43aa415eabe4a52571ba4cc0924941
Animesh Patcha <animesh.patcha@bigswitch.com>
2014-11-10 15:18:02 -0800

    Fix for show version

--- keywords/BsnCommon.py
+++ keywords/BsnCommon.py
@@ -553,7 +553,7 @@ class BsnCommon(object):
                 url = '/rest/v1/system/version'
                 if user == "admin":
                     try:
-                        t.node_reconnect(node='master')
+                        t.node_reconnect(node='master', user=str(user), password=password)
                         c.rest.get(url)
                         content = c.rest.content()
                         output_value = content[0]['controller']
@@ -568,11 +568,11 @@ class BsnCommon(object):
                         content = c_user.rest.content()
                         output_value = content[0]['controller']
                     except:
-                        t.node_reconnect(node='master')
+                        t.node_reconnect(node='master', user=str(user), password=password)
                         return False
                     else:
                         if local is True:
-                            t.node_reconnect(node='master')
+                            t.node_reconnect(node='master', user=str(user), password=password)
                         return output_value
 
             elif helpers.is_bigwire(n.platform()):
