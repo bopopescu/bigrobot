970d77fa902e928a705b83a2b0b2273d4e27a16d
Vui Le <vui.le@bigswitch.com>
2015-01-12 16:04:03 -0800

    - Support parallel execution of baseline DB population.

--- catalog/run_repopulate_build_baseline.sh
+++ catalog/run_repopulate_build_baseline.sh
@@ -14,6 +14,14 @@ if [ "$BUILD_NAME"x = x ]; then
     usage
 fi
 
+if [ "$TEST_CATALOG_LOG_DIR"x = x ]; then
+    ts=`date "+%Y-%m-%d_%H%M%S"`
+    dest=.data.$ts
+    mkdir $dest
+    export TEST_CATALOG_LOG_DIR=$dest
+    echo "TEST_CATALOG_LOG_DIR='$TEST_CATALOG_LOG_DIR'"
+fi
+
 BUILD_NAME="$BUILD_NAME" ./db_remove_build_baselines.py --y
 BUILD_NAME="$BUILD_NAME" time ./populate_db_dryrun.sh
 
