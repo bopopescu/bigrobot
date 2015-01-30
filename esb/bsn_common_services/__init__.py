5ff1babea6a310cacefa4b0e7d0cdebd55d8adc0
Vui Le <vui.le@bigswitch.com>
2015-01-30 12:33:17 -0800

    BSN ESB sercices - set log path

--- esb/bsn_common_services/__init__.py
+++ esb/bsn_common_services/__init__.py
@@ -11,7 +11,7 @@ sys.path.insert(0, bigrobot_path)
 import autobot.helpers as helpers
 helpers.set_env('IS_GOBOT', 'False')
 helpers.bigrobot_path(bigrobot_path)
-helpers.bigrobot_log_path('/tmp/bigrobot_esb_log')
+helpers.bigrobot_log_path('/home/bsn/bigrobot_esb_log')
 helpers.bigrobot_log_path_exec_instance(
                           helpers.bigrobot_log_path() +
                           "/" + os.path.dirname(__file__).split('/')[-1])
