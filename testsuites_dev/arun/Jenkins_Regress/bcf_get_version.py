5825b4d088fc6e893e96e53f1f30b463940cb506
Arun.Mallina <aruna.mallina@bigswitch.com>
2015-03-27 01:57:44 -0700

    add ability to read from bigrobot topo file

--- testsuites_dev/arun/Jenkins_Regress/bcf_get_version.py
+++ testsuites_dev/arun/Jenkins_Regress/bcf_get_version.py
@@ -12,6 +12,7 @@ import os
 import sys
 import re
 import argparse
+import yaml
 
 def get_version_string(args):
     controller = paramiko.SSHClient()
@@ -32,19 +33,31 @@ def prog_args():
     descr = """\
 Display Version String for Regression BUILD NAME setting
 
-Usage: bcf_get_version --controller_ip 10.9.16.44
+Usage: bcf_get_version --controller_ip 10.9.16.44 or
+       bcf_get_version --topo_file /var/temp.topo
 """
     parser = argparse.ArgumentParser(
                         prog='bcf_get_version',
                         formatter_class=argparse.RawDescriptionHelpFormatter,
                         description=descr)
     parser.add_argument('--controller_ip',
-                        help=("Version file"))
+                        help=("Controller_ip"))
+    parser.add_argument('--topo_file',
+                        help=("Bigrobot Topo file"))
     _args = parser.parse_args()
 
-    if _args.controller_ip == None:
-        print "Please provide Controller IP!!"
+    if _args.controller_ip == None and _args.topo_file == None:
+        print "Please provide Controller IP or topo file!!"
         sys.exit(1)
+    if _args.topo_file != None:
+        if os.path.exists(_args.topo_file):
+            stream = open(_args.topo_file, 'r')
+            topo_dict = yaml.load(stream)
+            _args.controller_ip = topo_dict['c1']['ip']
+        else:
+            print ("File %s does not exist" % args.topo_file)
+            sys.exit(1)
+
     return _args
 
 if __name__ == '__main__':
